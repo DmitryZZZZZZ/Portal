3b19753f7eeab027146ec63fe501a993a41e6f42
Dmitry Zaharov <98744363+Dimose75@users.noreply.github.com>
2023-04-24 00:02:10 +0400

    Initial commit

---
+++ news/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .views import PostsList, PostDetail, PostSearch, PostCreate, PostUpdate, PostDelete, upgrade_me, CategoryListView, \
+    subscribe
+
+urlpatterns = [
+    path('', PostsList.as_view(), name='post_list'),
+    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+    path('search/', PostSearch.as_view(), name='post_search'),
+    path('create/', PostCreate.as_view(), name='news_create'),
+    path('<int:pk>/edit/', PostUpdate.as_view(), name='news_edit'),
+    path('<int:pk>/delete/', PostDelete.as_view(), name='news_delete'),
+    path('articles/create/', PostCreate.as_view(), name='articles_create'),
+    path('articles/<int:pk>/edit/', PostUpdate.as_view(), name='articles_edit'),
+    path('articles/<int:pk>/delete/', PostDelete.as_view(), name='articles_delete'),
+    path('upgrade/', upgrade_me, name='upgrade'),
+    path('categories/<int:pk>', CategoryListView.as_view(), name='category_list'),
+    path('categories/<int:pk>/subscribe', subscribe, name='subscribe'),
+
+]
+
